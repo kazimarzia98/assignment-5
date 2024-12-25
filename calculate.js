f8171a8c229a6b23281adf79dd0b5efc5de6448d
Kazimarzia98 <Kazimarzia98@gmail.com>
2024-12-26 02:19:21 +0600

    main part done

---
+++ calculate.js
@@ -0,0 +1,64 @@
+document.getElementById('card-donate-btn').addEventListener('click', function () {
+    const inputAmount = parseFloat(document.getElementById('donate-amount').value);
+    const cardAmount = parseFloat(document.getElementById('card-amount').innerText);
+
+    if (inputAmount < 0 || isNaN(inputAmount)) {
+        alert('Failed to add money. Try again');
+    }
+    else {
+
+
+        const totalCardAmount = inputAmount + cardAmount;
+
+        document.getElementById('card-amount').innerText = totalCardAmount;
+
+        const balance = parseFloat(document.getElementById('balance').innerText);
+        const remainingBalance = balance - inputAmount;
+        console.log(remainingBalance);
+
+        document.getElementById('balance').innerText = remainingBalance;
+
+        const donationName = document.getElementById('donation-name').innerText;
+        const historyItem = document.createElement('div');
+        historyItem.className = 'bg-white border border-gray-200 rounded-md w-full lg:w-10/12 mx-auto py-7 px-5';
+
+        historyItem.innerHTML = `
+        <p class='text-lg font-style'>${inputAmount} Taka is Donated for ${donationName}</p>
+        <p class='text-md'>Date: ${new Date().toDateString()} ${new Date().toTimeString()}</p>
+        `
+        const historyContainer = document.getElementById('history-body');
+         
+        historyContainer.insertBefore(historyItem , historyContainer.firstChild);
+
+    }
+})
+
+// history tab
+
+const historyTab = document.getElementById('btn-history');
+const donateTab = document.getElementById('btn-donate');
+
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add('btn-style', 'py-0', 'hover:bg-green-300', 'border-0');
+    donateTab.classList.remove('btn-style', 'py-0', 'hover:bg-green-300');
+    donateTab.classList.add('border', 'border-gray-400', 'rounded-md', 'hover:bg-green-300')
+
+
+
+    document.getElementById('card-body').classList.add('hidden');
+    document.getElementById('history-body').classList.remove('hidden');
+})
+
+// donate Tab
+
+donateTab.addEventListener('click', function(){
+    donateTab.classList.add('btn-style', 'py-0', 'hover:bg-green-300', 'border-0');
+    historyTab.classList.remove('btn-style', 'py-0', 'hover:bg-green-400');
+    historyTab.classList.add('border', 'border-gray-400', 'rounded-md', 'hover:bg-greeen-300');
+
+    document.getElementById('history-body').classList.add('hidden');
+    document.getElementById('card-body').classList.remove('hidden');
+
+
+
+})
